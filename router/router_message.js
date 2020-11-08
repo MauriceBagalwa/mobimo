46b692c007d01f71d8fb860fb00c988461ed4fdd
Maurice Bagalwa <mauricebagalwa009@gmail.com>
2020-11-08 16:07:44 +0100

    modification du chemin

--- router/router_message.js
+++ router/router_message.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const messageCtr = require("../controller/mesage");
 
 router.get("/", messageCtr.messages);
+router.put("/", messageCtr.message);
 
 module.exports = router;
