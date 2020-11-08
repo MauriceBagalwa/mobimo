ceb8de1f7bef3592f81dbb4df9976e4ed7f80e84
Maurice Bagalwa <mauricebagalwa009@gmail.com>
2020-11-08 16:07:04 +0100

    modification du controller

--- controller/mesage.js
+++ controller/mesage.js
@@ -1,14 +1,31 @@
 const db = require("../models");
+const shortId = require("shortid");
 module.exports = {
   messages: async (req, res, next) => {
-    res.send("Hey mobimo...");
-    // db.message
-    //   .findAndCountAll({ offset: 5, limit: 5 })
-    //   .then((find) => {
-    //     res.status(200).json(find);
-    //   })
-    //   .catch((error) => {
-    //     next(error);
-    //   });
+    db.message
+      .findAndCountAll({ offset: 5, limit: 5 })
+      .then((find) => {
+        res.status(200).json(find);
+      })
+      .catch((error) => {
+        next(error);
+      });
+  },
+  message: async (req, res, next) => {
+    const item = {
+      id: shortId.generate(),
+      numero: req.body.numero,
+      reseau: req.body.reseau,
+      message: req.body.message,
+    };
+    db.message
+      .create(item)
+      .then((create) => {
+        res.status(200).json(create);
+      })
+      .catch((error) => {
+        console.log(error);
+        next(error);
+      });
   },
 };
