f6151fae62849d769bec39ed3e1e812f1bc5effe
Maurice Bagalwa <mauricebagalwa009@gmail.com>
2020-11-09 11:00:08 +0100

    ci(path): add path

--- router/router_transaction.js
+++ router/router_transaction.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const tarnsactionCtr = require("../controller/transactionController");
 
 router.get("/", tarnsactionCtr.transactions);
+router.get("/id", tarnsactionCtr.isUse);
 router.put("/", tarnsactionCtr.transaction);
 
 module.exports = router;
