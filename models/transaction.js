a84e09f7e03e9f16ba5c6d2e752fbdb61d40b32f
Maurice Bagalwa <mauricebagalwa009@gmail.com>
2020-11-09 04:42:17 +0100

    ci(models.trasanction): upadte variables

--- models/transaction.js
+++ models/transaction.js
@@ -39,7 +39,7 @@ module.exports = function (sequelize, DataTypes) {
         type: DataTypes.TEXT,
         allowNull: false,
       },
-      date: {
+      dateReception: {
         type: DataTypes.DATE,
         defaultValue: DataTypes.NOW,
       },
