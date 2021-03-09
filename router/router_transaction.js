7976533028533034648b246dabb5a4c1de0daff3
Maurice Bagalwa <mauricebagalwa009@gmail.com>
2021-03-09 13:04:20 +0100

    find(router): add router to send Mssg

--- router/router_transaction.js
+++ router/router_transaction.js
@@ -4,6 +4,7 @@ const tarnsactionCtr = require("../controller/transactionController");
 router.get("/", tarnsactionCtr.transactions);
 router.get("/id", tarnsactionCtr.isUse);
 router.get("/details", tarnsactionCtr.details);
+router.post("/send_mssg", tarnsactionCtr.sendMssg);
 router.put("/", tarnsactionCtr.transaction);
 
 module.exports = router;
